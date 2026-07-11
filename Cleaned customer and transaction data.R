582a7b7159ebf74a7aeaddb5044bed182264c692
koleladan <isaiahodan@gmail.com>
2026-07-11 19:51:08 +0300

    cleaned customer and transaction data done

---
+++ Cleaned customer and transaction data.R
@@ -0,0 +1,10 @@
+git config --global user.name"koleladan"
+git config --global user.email "isaiahodan@gmail.com"
+git remote add origin https://github.com/koleladan/R_customer_churn_analysis
+git branch -M main
+README.md
+git add .
+git commit -m "initial commit"
+git push -u origin main
+
+
